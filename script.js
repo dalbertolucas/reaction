564322f25e400b54ef113b844d4174d027d2083d
dalbertolucas <92748355+dalbertolucas@users.noreply.github.com>
2025-12-17 00:55:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const GRID_SIZE = 6;
 const TOTAL_CELLS = GRID_SIZE * GRID_SIZE;
 
 const settings = {
-  easy:   { litCount: 1, litMs: 1600 },
-  medium: { litCount: 2, litMs: 1300 },
-  hard:   { litCount: 2, litMs: 900 },
+  easy:   { litCount: 1, litMs: 1000 },
+  medium: { litCount: 2, litMs: 750 },
+  hard:   { litCount: 2, litMs: 450 },
 };
 
 const gridEl = document.getElementById("grid");
@@ -199,4 +199,5 @@ document.querySelectorAll(".diff-btn").forEach(btn => {
 });
 
 buildGrid();
+
 
